477465da751430b01aa217731d1e24f7fa502511
OscarPV <oscarpoloniov@gmail.com>
2021-10-13 12:48:26 +0200

    cambio de tiles y lvl

--- Game/02-Bubble/02-Bubble/Game.h
+++ Game/02-Bubble/02-Bubble/Game.h
@@ -5,8 +5,8 @@
 #include "Scene.h"
 
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 1350
+#define SCREEN_HEIGHT 1080
 
 
 // Game is a singleton (a class with a single instance) that represents our whole application
